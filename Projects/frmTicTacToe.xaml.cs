7ff7f799ff8003d1e3f2a90de970e2bad11919b6
PlGGS <picklepig7@comcast.net>
2018-01-31 15:16:44 -0600

    Refactored Tic Tac Toe

--- Projects/frmTicTacToe.xaml.cs
+++ Projects/frmTicTacToe.xaml.cs
@@ -18,6 +18,7 @@ namespace Projects
     public partial class frmTicTacToe : Window
     {
         public bool p1Turn = true;
+        public int turns = 0;
 
         public frmTicTacToe()
         {
@@ -31,19 +32,21 @@ namespace Projects
 
         private void TurnText()
         {
-            if (p1Turn == false)
-            {
-                lblText.Content = "Player 2's turn!";
-            }
-            else
+            if (p1Turn)
             {
                 lblText.Content = "Player 1's turn!";
             }
+            else
+            {
+                lblText.Content = "Player 2's turn!";
+            }
         }
 
-        private void lblTopLeft_MouseUp(object sender, MouseButtonEventArgs e)
+        private void lbl_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            if (lblTopLeft.Content.ToString() == "")
+            Label lbl = (Label)sender;
+
+            if (lbl.Content.ToString() == "")
             {
                 if (p1Turn)
                 {
@@ -56,142 +59,16 @@ namespace Projects
                 p1Turn = !p1Turn;
                 TurnText();
             }
-        }
 
-        private void lblTop_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            if (lblTop.Content.ToString() == "")
+            if (turns >= 5)
             {
-                if (p1Turn)
-                {
-                    lblTop.Content = " X";
-                }
-                else
-                {
-                    lblTop.Content = " O";
-                }
-                p1Turn = !p1Turn;
-                TurnText();
+                CheckWinner();
             }
         }
 
-        private void lblTopRight_MouseUp(object sender, MouseButtonEventArgs e)
+        private void CheckWinner()
         {
-            if (lblTopRight.Content.ToString() == "")
-            {
-                if (p1Turn)
-                {
-                    lblTopRight.Content = " X";
-                }
-                else
-                {
-                    lblTopRight.Content = " O";
-                }
-                p1Turn = !p1Turn;
-                TurnText();
-            }
-        }
-
-        private void lblLeft_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            if (lblLeft.Content.ToString() == "")
-            {
-                if (p1Turn)
-                {
-                    lblLeft.Content = " X";
-                }
-                else
-                {
-                    lblLeft.Content = " O";
-                }
-                p1Turn = !p1Turn;
-                TurnText();
-            }
-        }
-
-        private void lblMid_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            if (lblMid.Content.ToString() == "")
-            {
-                if (p1Turn)
-                {
-                    lblMid.Content = " X";
-                }
-                else
-                {
-                    lblMid.Content = " O";
-                }
-                p1Turn = !p1Turn;
-                TurnText();
-            }
-        }
-
-        private void lblRight_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            if (lblRight.Content.ToString() == "")
-            {
-                if (p1Turn)
-                {
-                    lblRight.Content = " X";
-                }
-                else
-                {
-                    lblRight.Content = " O";
-                }
-                p1Turn = !p1Turn;
-                TurnText();
-            }
-        }
-
-        private void lblBotLeft_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            if (lblBotLeft.Content.ToString() == "")
-            {
-                if (p1Turn)
-                {
-                    lblBotLeft.Content = " X";
-                }
-                else
-                {
-                    lblBotLeft.Content = " O";
-                }
-                p1Turn = !p1Turn;
-                TurnText();
-            }
-        }
-
-        private void lblBot_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            if (lblBot.Content.ToString() == "")
-            {
-                if (p1Turn)
-                {
-                    lblBot.Content = " X";
-                }
-                else
-                {
-                    lblBot.Content = " O";
-                }
-                p1Turn = !p1Turn;
-                TurnText();
-            }
-        }
-
-        private void lblBotRight_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            if (lblBotRight.Content.ToString() == "")
-            {
-                if (p1Turn)
-                {
-                    lblBotRight.Content = " X";
-                }
-                else
-                {
-                    lblBotRight.Content = " O";
-                }
-                p1Turn = !p1Turn;
-                TurnText();
-            }
+            //TODO check winner
         }
     }
 }
