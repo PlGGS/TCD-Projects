d40ed404444fdece44ba355642d204bfeed46ece
PlGGS <picklepig7@comcast.net>
2018-02-01 10:33:13 -0600

    Updated CheckWinner

--- Projects/frmTicTacToe.xaml.cs
+++ Projects/frmTicTacToe.xaml.cs
@@ -31,20 +31,30 @@ namespace Projects
         /// </summary>
         void SetSpots()
         {
-            spots[0, 0] = (char)lblTopLeft.Content;
-            spots[1, 0] = (char)lblTop.Content;
-            spots[2, 0] = (char)lblTopRight.Content;
-            spots[0, 1] = (char)lblLeft.Content;
-            spots[1, 1] = (char)lblMid.Content;
-            spots[2, 1] = (char)lblRight.Content;
-            spots[0, 2] = (char)lblBotLeft.Content;
-            spots[1, 2] = (char)lblBot.Content;
-            spots[2, 2] = (char)lblBotRight.Content;
+            if (lblTopLeft.Content.ToString() != "") spots[0, 0] = Convert.ToChar(lblTopLeft.Content.ToString().Substring(1, 1));
+            if (lblTop.Content.ToString() != "") spots[1, 0] = Convert.ToChar(lblTop.Content.ToString().Substring(1, 1));
+            if (lblTopRight.Content.ToString() != "") spots[2, 0] = Convert.ToChar(lblTopRight.Content.ToString().Substring(1, 1));
+            if (lblLeft.Content.ToString() != "") spots[0, 1] = Convert.ToChar(lblLeft.Content.ToString().Substring(1, 1));
+            if (lblMid.Content.ToString() != "") spots[1, 1] = Convert.ToChar(lblMid.Content.ToString().Substring(1, 1));
+            if (lblRight.Content.ToString() != "") spots[2, 1] = Convert.ToChar(lblRight.Content.ToString().Substring(1, 1));
+            if (lblBotLeft.Content.ToString() != "") spots[0, 2] = Convert.ToChar(lblBotLeft.Content.ToString().Substring(1, 1));
+            if (lblBot.Content.ToString() != "") spots[1, 2] = Convert.ToChar(lblBot.Content.ToString().Substring(1, 1));
+            if (lblBotRight.Content.ToString() != "") spots[2, 2] = Convert.ToChar(lblBotRight.Content.ToString().Substring(1, 1));
+        }
+
+        void PrintSpots()
+        {
+            foreach (char spot in spots)
+            {
+                Console.Write($"{spot} ");
+            }
+
+            Console.WriteLine();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            
+            SetSpots();
         }
 
         private void TurnText()
@@ -78,6 +88,8 @@ namespace Projects
                 TurnText();
             }
 
+            PrintSpots();
+
             if (turns >= 5)
             {
                 CheckWinner();
@@ -86,7 +98,57 @@ namespace Projects
 
         private void CheckWinner()
         {
-            //TODO check winner
+            int xCount = 0;
+            int oCount = 0;
+
+            //check rows
+            for (int y = 0; y < 3; y++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    if (spots[x, y] != '\0' && spots[x, y] == 'X')
+                    {
+                        xCount++;
+                    }
+
+                    if (spots[x, y] != '\0' && spots[x, y] == 'O')
+                    {
+                        oCount++;
+                    }
+                }
+            }
+            
+            //check columns
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    if (spots[x, y] != '\0' && spots[x, y] == 'X')
+                    {
+                        xCount++;
+                    }
+
+                    if (spots[x, y] != '\0' && spots[x, y] == 'O')
+                    {
+                        oCount++;
+                    }
+                }
+            }
+
+            //TODO check diagonally
+
+            if (xCount == 3)
+            {
+                lblText.Content = "Player 1 has won!";
+            }
+            else if (oCount == 3)
+            {
+                lblText.Content = "Player 2 has won!";
+            }
+            else if (turns >= 9)
+            {
+                lblText.Content = "It's a cat's game!";
+            }
         }
     }
 }
