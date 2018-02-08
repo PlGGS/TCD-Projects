44f28c46cfb7a48fa5fce6ecaf61dd09158b342b
PlGGS <picklepig7@comcast.net>
2018-02-08 10:01:24 -0600

    Added 2nd and 3rd triangles

--- Projects/frmNested.xaml.cs
+++ Projects/frmNested.xaml.cs
@@ -27,8 +27,15 @@ namespace Projects
         private void btnRectangle_Click(object sender, RoutedEventArgs e)
         {
             txbMain.Text = "";
-            
 
+            for (int i = 0; i < 10; i++)
+            {
+                for (int o = 0; o < 10; o++)
+                {
+                    txbMain.Text += "B";
+                }
+                txbMain.Text += "\n";
+            }
         }
 
         private void btnTriangle1_Click(object sender, RoutedEventArgs e)
@@ -48,8 +55,39 @@ namespace Projects
         private void btnTriangle2_Click(object sender, RoutedEventArgs e)
         {
             txbMain.Text = "";
-            
 
+            for (int i = 10; i > 0; i--)
+            {
+                for (int s = 10; s > i; s--)
+                {
+                    txbMain.Text += " ";
+                }
+
+                for (int b = 0; b < i; b++)
+                {
+                    txbMain.Text += "B";
+                }
+                txbMain.Text += "\n";
+            }
+        }
+
+        private void btnTriangle3_Click(object sender, RoutedEventArgs e)
+        {
+            txbMain.Text = "";
+
+            for (int i = 10; i > 0; i--)
+            {
+                for (int s = 10; s > i; s--)
+                {
+                    txbMain.Text += "  ";
+                }
+
+                for (int b = 0; b < i; b++)
+                {
+                    txbMain.Text += "B";
+                }
+                txbMain.Text += "\n";
+            }
         }
     }
 }
